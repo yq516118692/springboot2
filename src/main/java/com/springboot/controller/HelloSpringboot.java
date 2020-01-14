0d9ec1cbede75990227d9d39d680d1365b3c8734
yinq <2424813080@qq.com>
2020-01-15 00:22:49 +0800

    第一次提交

---
+++ src/main/java/com/springboot/controller/HelloSpringboot.java
@@ -0,0 +1,16 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloSpringboot {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello()
+	{
+		return "hellow world";
+	}
+}
